b72ba38ecdbaceb34bf95037d9d2742257a7a799
omarkhaledabdo <omarkhaledabdo@gmail.com>
2018-04-08 16:11:30 +0200

    add databse integration

--- src/ai_models/rl_model.py
+++ src/ai_models/rl_model.py
@@ -1,22 +1,22 @@
+# import sys
+# sys.path.append('../')
+
+import db.rl_model as replay_memory
 
 class Model:
 
     def get_action(self, time_stamp):
-        return 'R'
+        return replay_memory.query_action(time_stamp)
+
+    def add_memory(self, time_stamp, current_state):
+        action = self.create_action(time_stamp, current_state)
+        print action 
+        replay_memory.insert_transition(time_stamp, current_state, action)
 
     def create_action (self, time_stamp, current_state):
         return 'R'
 
-    def add_memory(self, time_stamp, current_state):
-        action = self.create_action(time_stamp, current_state)
-        #database insert
-
-    def update_memory(self, time_stamp, reward=None, next_state=None):
-        if reward:
-            print 'update reward in db'
-            #database update
-        if next_state:
-            print 'update reward in db'
-            #database update
+    def update_previous_memory(self, time_stamp, reward=None, next_state=None):
+      replay_memory.update_transition(time_stamp, reward, next_state)
 
 model = Model()
